d3d7cda69afe67f22886054c3edafa1c4915237f
Shayster7 <shayqian7@gmail.com>
2025-09-14 16:28:31 +1000

    small refactoring, updating files

--- src/promotions.ts
+++ src/promotions.ts
@@ -3,13 +3,12 @@ import { CartItem, Promotion, SKU } from './types';
 
 /**
  * A promotion that applies an 'X for Y' deal.
- * e.g., 3 for 2, where x=3 and y=2.
  */
 export class XForYDealPromotion implements Promotion {
   constructor(private sku: SKU, private x: number, private y: number) {}
 
   apply(cartItems: CartItem[]): void {
-    const item = cartItems.find(i => i.sku === this.sku);
+    const item = cartItems.find(i => i.sku === this.sku); // double equal?; this?
 
     if (item && item.quantity >= this.x) {
       const dealQuantity = Math.floor(item.quantity / this.x);
@@ -21,7 +20,6 @@ export class XForYDealPromotion implements Promotion {
 
 /**
  * A promotion that applies a bulk discount.
- * e.g., price drops to a new price if quantity is over a certain threshold.
  */
 export class BulkDiscountPromotion implements Promotion {
   constructor(private sku: SKU, private threshold: number, private discountedPrice: number) {}
